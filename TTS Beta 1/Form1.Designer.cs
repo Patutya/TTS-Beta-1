c9d12d8bf70dfe0ffea486f2d76733983bb6d81f
Natia <Natia@DESKTOP-D6CFAHF>
2023-11-23 15:12:54 +0400

    Add project files.

---
+++ TTS Beta 1/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace TTS_Beta_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.noVoice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // noVoice
+            // 
+            this.noVoice.Location = new System.Drawing.Point(21, 196);
+            this.noVoice.Multiline = true;
+            this.noVoice.Name = "noVoice";
+            this.noVoice.Size = new System.Drawing.Size(1098, 407);
+            this.noVoice.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(657, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TTS BETA V0.1 ეს პროგრამა ჯერჯერობით არის ტესტირების ფაზაში და ჯერკიდევ დახვეწა ჭ" +
+    "ირდება\r\nგთხოვთ გაითვალისწინოთ რომ ეს არ არის საბოლოო ვერსია.\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(918, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 175);
+            this.label2.TabIndex = 2;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(21, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(866, 64);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ჩასართავად ჩაწერეთ ქვემოთ მოცემულ ტექსტურ ყუთში სასურველი \r\nწინადადება და ამის შე" +
+    "მდეგ დააჭირეთ ხმის ჩართვას.";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1218, 549);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 54);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ხმის \r\nჩართვა";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1334, 632);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.noVoice);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox noVoice;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ TTS Beta 1/Form1.cs
@@ -0,0 +1,196 @@
+﻿namespace TTS_Beta_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void cooloff(int millisecs)
+        {
+            DateTime Tthen = DateTime.Now;
+            do
+            {
+                Application.DoEvents();
+            } while (Tthen.AddMilliseconds(millisecs) > DateTime.Now);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Media.SoundPlayer g = new System.Media.SoundPlayer(@"c:\G.wav");
+            System.Media.SoundPlayer d = new System.Media.SoundPlayer(@"c:\D.wav");
+            System.Media.SoundPlayer ea = new System.Media.SoundPlayer(@"c:\E.wav");
+            System.Media.SoundPlayer b = new System.Media.SoundPlayer(@"c:\B.wav");
+            System.Media.SoundPlayer a = new System.Media.SoundPlayer(@"c:\A.wav");
+            string voice = noVoice.Text;
+            int count = voice.Length;
+            int sp = 350;
+            for (int ji = 0; ji < count; ji++)
+            {
+                switch (voice[ji])
+                {
+                    case ' ':
+                        break;
+                    case 'ა':
+
+                        a.Play();
+                        break;
+                    case 'ბ':
+                        b.Play();
+                        break;
+                    case 'გ':
+
+                        g.Play();
+                        break;
+                    case 'დ':
+
+                        d.Play();
+                        break;
+                    case 'ე':
+                        ea.Play();
+                        Thread.Sleep(150);
+
+                        break;
+                        
+
+                }
+                cooloff(sp);
+
+            }
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ /* case 'ვ':
+                             System.Media.SoundPlayer v = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             v.Play();
+                             break;
+                         case 'ზ':
+                             System.Media.SoundPlayer z = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             z.Play();
+                             break;
+                         case 'თ':
+                             System.Media.SoundPlayer th = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             th.Play();
+                             break;
+                         case 'ი':
+                             System.Media.SoundPlayer i = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             i.Play();
+                             break;
+                         case'კ':
+                             System.Media.SoundPlayer k = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             k.Play();
+                             break;
+                         case 'ყ':
+                             System.Media.SoundPlayer ky = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             ky.Play();
+                             break;
+                         case 'ლ':
+                             System.Media.SoundPlayer l = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             l.Play();
+                             break;
+                         case 'მ':
+                             System.Media.SoundPlayer m = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             m.Play();
+                             break;
+                         case 'ნ':
+                             System.Media.SoundPlayer n = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             n.Play();
+                             break;
+                         case 'ო':
+                             System.Media.SoundPlayer o = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             o.Play();
+                             break;
+                         case 'პ':
+                             System.Media.SoundPlayer p = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             p.Play();
+                             break;
+                         case 'ჟ':
+                             System.Media.SoundPlayer zh = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             zh.Play();
+                             break;
+                         case 'რ':
+                             System.Media.SoundPlayer r = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             r.Play();
+                             break;
+                         case 'ს':
+                             System.Media.SoundPlayer s = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             s.Play();
+                             break;
+                         case 'ტ':
+                             System.Media.SoundPlayer t = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             t.Play();
+                             break;
+                         case 'უ':
+                             System.Media.SoundPlayer u = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             u.Play();
+                             break;
+                         case 'ფ':
+                             System.Media.SoundPlayer f = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             f.Play();
+                             break;
+                         case 'ქ':
+                             System.Media.SoundPlayer q = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             q.Play();
+                             break;
+                         case 'ღ':
+                             System.Media.SoundPlayer gh = new System.Media.SoundPlayer(@"c:\mywavfil e.wav");
+                             gh.Play();
+                             break;
+                         case 'შ':
+                             System.Media.SoundPlayer sh = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             sh.Play();
+                             break;
+                         case 'ჩ':
+                             System.Media.SoundPlayer ch = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             ch.Play();
+                             break;
+                         case 'ც':
+                             System.Media.SoundPlayer c = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             c.Play();
+                             break;
+                         case 'ძ':
+                             System.Media.SoundPlayer dz = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             dz.Play();
+                             break;
+                         case 'წ':
+                             System.Media.SoundPlayer w = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             w.Play();
+                             break;
+                         case 'ჭ':
+                             System.Media.SoundPlayer tch = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             tch.Play();
+                             break;
+                         case 'ხ':
+                             System.Media.SoundPlayer x = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             x.Play();
+                             break;
+                         case 'ჯ':
+                             System.Media.SoundPlayer j = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             j.Play();
+                             break;
+                         case 'ჰ':
+                             System.Media.SoundPlayer h = new System.Media.SoundPlayer(@"c:\mywavfile.wav");
+                             h.Play();
+                             break;*/
+*/
